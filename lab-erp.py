56ab124e945f587e2af1fcd1b04a4974d92a18e4
Augusto <augustovmagro@gmail.com>
2025-03-08 01:05:23 -0300

    08/03/2025 01:04 Augusto: Commiting sidebar and dashboard page

--- lab-erp.py
+++ lab-erp.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # return render_template('home/home.html')
     return render_template('home/home.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard/dashboard.html')
 
 app.run()
---
+++ static/js.js
@@ -0,0 +1,7 @@
+let bt = document.getElementById('dashboard');
+let idPersist = ''
+
+function changeBackgroundButtonSidebar(id) {
+    idPersist = id
+    document.getElementById(idPersist).style.backgroundColor = '#212529'
+}
